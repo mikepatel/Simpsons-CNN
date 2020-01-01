41d7e0970a3845fdb2a17a75ef618276b2cfff01
Michael Patel <michael.patel94@gmail.com>
2020-01-01 18:34:09 -0500

    Created dict for cats, values

--- app.py
+++ app.py
@@ -42,6 +42,18 @@ if __name__ == "__main__":
     # delete 'predictions' sub-directory
     delete_dir(os.path.join(temp_dir, "predictions"))
 
+    # mapping between Simpsons character and a numeric categorical label
+    char2num = {
+        "Bart Simpson": 0,
+        "Homer Simpson": 1
+    }
+
+    # reverse mapping
+    num2char = {v: k for k, v in char2num.items()}
+
+    # number of output classes
+    num_classes = len(char2num)
+
     # ----- PREDICT ----- #
     # load model
     filepath = os.path.join(temp_dir, "saved_model.h5")
@@ -60,12 +72,37 @@ if __name__ == "__main__":
 
         # resize image
         resized_image = image.resize((64, 64))
+        images.append(np.array(resized_image))
 
-        # normalize image
-        image = np.array(resized_image).astype(np.float32) / 255.0
-
-        # add to list
-        images.append(image)
+    # normalize images
+    images = np.array(images).astype(np.float32) / 255.0
 
     # predict using model
+    #images = np.expand_dims(images, 0)
+    predictions = model.predict(images)
+    #print(predictions)
+
+    text = []
+    for i in range(len(predictions)):
+        image_text = []
+        
+        for j in range(num_classes):
+            t = {
+                "name": num2char[j],
+                "value": predictions[i][j]
+            }
+            image_text.append(t)
+        text.append(image_text)
+
+        """
+        category_int = np.argmax(predictions[i])
+        t = {
+            "category": num2char[category_int],
+            "value": predictions[i][]
+        }
+        print(num2char[category_int])
+        """
+
+    print(text)
+
     # create gif

af7e21972ee76452d6133df6f6527ed8885c7f0d
Michael Patel <michael.patel94@gmail.com>
2020-01-07 17:25:25 -0500

    Predicting 1 at a time

--- app.py
+++ app.py
@@ -64,12 +64,102 @@ if __name__ == "__main__":
     filepath = os.path.join(temp_dir, "saved_model.h5")
     model = tf.keras.models.load_model(filepath)
 
-    # read in test dataset
-    """
-    homer_test_dir = os.path.join(os.getcwd(), "data\\Test\\Homer Simpson")
-    image_files_pattern = homer_test_dir + "\\*.jpg"
+    # read in images and predict one-by-one
+    for character in char2num:
+        image_files_pattern = os.path.join("data\\Test\\" + character) + "\\*.jpg"
+        filenames = glob.glob(image_files_pattern)
+
+        for f in filenames:
+            image = Image.open(f)
+            original_image = image
+
+            # resize image
+            image = image.resize((64, 64))
+
+            # normalize image
+            image = np.array(image).astype(np.float32) / 255.0
+
+            # reshape: (1, 64, 64, 3)
+            image = np.expand_dims(image, 0)
+
+            # model prediction
+            prediction = model.predict(image)
+            #print(f'Prediction: {prediction}')
+
+            """
+            print(f'\n# ----- PREDICTIONS ----- #')
+            # prediction as distribution
+            prediction_distribution = prediction
+            print(f'Prediction as distribution: {prediction_distribution}')
+
+            # prediction as int
+            prediction = np.argmax(prediction_distribution)
+            print(f'Prediction as numerical category: {prediction}')
+
+            # prediction as text
+            prediction = num2char[prediction]
+            print(f'Prediction as text: {prediction}')
+            """
+
+            # create prediction text
+            #print(f'\n# ----- PREDICTION TEXT BLOCK ----- #')
+            text = []
+
+            for i in range(num_classes):
+                x = {"name": num2char[i],
+                     "value": prediction[0][i]
+                     }
+                text.append(x)
+
+            # sort predictions in descending order
+            text = sorted(text, key=lambda i: (i["value"]), reverse=True)
+
+            # build text block over image
+            z = []
+            for i in text:
+                x = f'{i["name"]}: {i["value"]:.6f}'
+                z.append(x)
+
+            z = "\n".join(z)
+            #print(f'{z}')
+
+            # write prediction text over image
+            # get image filename
+            name = f.split("\\")[-1]
+
+            #image_path = os.path.join(os.getcwd(), "data\\Training\\Homer Simpson\\pic_2245.jpg")
+            #image = Image.open(image_path)
+            draw = ImageDraw.Draw(original_image)
+            font = ImageFont.truetype("arial.ttf", 8)
+            draw.text((0, 0), z, font=font)
+            original_image.save(predictions_dir + "\\pred_" + name)
+
+    # ----- VISUALIZATION ----- #
+    # create gif
+    gif_filename = os.path.join(predictions_dir, "predictions.gif")
+
+    # get all predicted images
+    image_files_pattern = predictions_dir + "\\*.jpg"
     filenames = glob.glob(image_files_pattern)
+
+    # shuffle filenames
+
+    # write all images to gif
+    with imageio.get_writer(gif_filename, mode="I", fps=0.8) as writer:  # 'I' for multiple images
+        for f in filenames:
+            image = imageio.imread(f)
+            writer.append_data(image)
+
+    # delete all individual predicted images
+    for f in filenames:
+        if f.endswith(".jpg"):
+            os.remove(f)
     """
+
+    # read in test dataset
+    #homer_test_dir = os.path.join(os.getcwd(), "data\\Test\\Homer Simpson")
+    #image_files_pattern = homer_test_dir + "\\*.jpg"
+    #filenames = glob.glob(image_files_pattern)
 
     # preprocess images
     images = []
@@ -150,7 +240,10 @@ if __name__ == "__main__":
             image = imageio.imread(f)
             writer.append_data(image)
 
+
     # delete all individual predicted images
-    for f in filenames:
-        if f.endswith(".jpg"):
-            os.remove(f)
+    #for f in filenames:
+    #    if f.endswith(".jpg"):
+    #        os.remove(f)
+    
+    """
